5a94752b081918b24cb350f71fad504351c8a180
Halfdan Hauch Jensen <halfdanhauchjensen@gmail.com>
2021-10-28 01:19:04 +0200

    updated

--- Sonic_zoom_box/Wind_sensor/anemometer_AIR_serial_protocol/anemometer_AIR_serial_protocol.ino
+++ Sonic_zoom_box/Wind_sensor/anemometer_AIR_serial_protocol/anemometer_AIR_serial_protocol.ino
@@ -1,6 +1,18 @@
 /*
-Connect the voltage signal wire to Arduino analog interface:
-Yellow Cable<---->A0
+ * Code by AIR LAB @halfdan hauch jensen (halj@itu.dk)
+ * 
+ * Based on example from this page:
+ * https://wiki.dfrobot.com/Wind_Speed_Sensor_Voltage_Type_0-5V__SKU_SEN0170
+ * 
+ * Connect the voltage signal wire to Arduino analog interface: 
+ * 
+ * Wiring:
+ * Yellow cable <----> A0
+ * Black cable  <----> GND
+ * Red cable    <----> vin
+ * 
+ * Using the AIR LAB serial protocol that can be found here:
+ * https://github.com/airlabitu/sandbox/tree/master/AIR_LAB_serial_protocol
 */
 
  void setup()
@@ -10,15 +22,26 @@ Yellow Cable<---->A0
 
  void loop()
  {
+   
    int sensorValue = analogRead(A0);
    float outvoltage = sensorValue * (5.0 / 1023.0);
-   Serial.print("outvoltage = ");
-   Serial.print(outvoltage);
-   Serial.println("V");
-   int Level = 6*outvoltage;//The level of wind speed is proportional to the output voltage.
-   Serial.print("wind speed is ");
-   Serial.print(Level);
-   Serial.println(" level now");
-   Serial.println();
-   delay(500);
+
+   // sensor voltage
+   
+   // outValue is 100 times larger than original, in ordser to send the value as 'int' without significant data loss
+   int outValue = int(outvoltage*100);                                      
+   
+   Serial.print(1); // channel
+   Serial.print('c');
+   Serial.print(outValue); // value
+   Serial.print('w');
+
+   // wind level
+   int level = 6*outvoltage;//The level of wind speed is proportional to the output voltage.
+   Serial.print(2); // channel
+   Serial.print('c');
+   Serial.print(level); // value
+   Serial.print('w');
+   
+   delay(50); // small delay for minimizing data stream load
  }
